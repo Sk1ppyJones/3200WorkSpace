0e6b891671a6e9a04822c2439ce0aaa99279960f
Casey Jones <155927499+Sk1ppyJones@users.noreply.github.com>
2026-03-26 13:02:35 -0600

    Week 11 D2

--- sessiondemo/profiles/urls.py
+++ sessiondemo/profiles/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.CreateProfileView.as_view(), name='create_profile'),
+    path('success', views.SuccessView.as_view(), name='success'),
+    path('profiles', views.UserProfilesView.as_view(), name='user-profiles')
 ]
--- sessiondemo/profiles/views.py
+++ sessiondemo/profiles/views.py
@@ -1,15 +1,40 @@
 from urllib import request
 
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.views import View
+from django.views.generic import CreateView, TemplateView, ListView
+
+from .forms import CreateProfileForm
+from .models import Profile
 
 # Create your views here.
 
 
-class CreateProfileView(View):
-    def get(self, request):
-        return render(request, 'profiles/create_profile.html')
+class CreateProfileView(CreateView):
+    model = Profile
+    fields = ['name', 'image']
+    template_name = 'profiles/create_profile.html'
+    success_url = '/success'
 
-    def post(self, request):
-        print(request.FILES['image'])
-        return render(request, 'profiles/create_profile.html')
+    # def get(self, request):
+    #     form = CreateProfileForm()
+    #     profiles = Profile.objects.all()
+    #     return render(request, 'profiles/create_profile.html', {"form": form, "profiles": profiles})
+
+    # def post(self, request):
+    #     submitted_form = CreateProfileForm(request.POST, request.FILES)
+    #     if submitted_form.is_valid():
+    #         profile = Profile(
+    #             image=submitted_form.cleaned_data['image'], name=submitted_form.cleaned_data['name'])
+    #         profile.save()
+    #         return redirect('create-profile')
+    #     return render(request, 'profiles/create_profile.html', {"form": submitted_form})
+
+
+class SuccessView(TemplateView):
+    template_name = 'profiles/success.html'
+
+class UserProfilesView(ListView):
+    model = Profile
+    template_name = 'profiles/user_profiles.html'
+    context_object_name = 'profiles'
